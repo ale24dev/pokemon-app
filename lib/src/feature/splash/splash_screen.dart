a0c2828c5b51aa169277bc65a06be588ddccb546
ale24dev <ale.24dev@gmail.com>
2023-07-23 02:37:54 -0400

    splash screen and login implemented

--- lib/src/feature/splash/splash_screen.dart
+++ lib/src/feature/splash/splash_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:test_pokemon_app/src/feature/splash/controller/splash_controller.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 800)).then((value) {
+      SplashController.checkLogin(context);
+    });
     return const Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Cargando...'),
+            SizedBox.square(
+              dimension: 10,
+            ),
+            CircularProgressIndicator()
+          ],
+        ),
       ),
     );
   }
